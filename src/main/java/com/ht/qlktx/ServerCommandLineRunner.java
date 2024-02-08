f32aac550183b243228d6d5ead11285e34ef4821
hoanthui <hoanthui123@gmail.com>
2024-02-08 21:44:12 +0700

    feat: add seed data

--- src/main/java/com/ht/qlktx/ServerCommandLineRunner.java
+++ src/main/java/com/ht/qlktx/ServerCommandLineRunner.java
@@ -4,10 +4,13 @@ import com.ht.qlktx.entities.User;
 import com.ht.qlktx.enums.Role;
 import com.ht.qlktx.enums.Sex;
 import com.ht.qlktx.modules.users.UserRepository;
+import com.ht.qlktx.utils.Helper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -17,6 +20,15 @@ public class ServerCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        List<User> students = Helper.createSeedStudents();
+        List<User> staffs = Helper.createSeedStaffs();
+
+        if (userRepository.count() == 0 && students != null && staffs != null) {
+            staffs.forEach(staff -> staff.setPassword(passwordEncoder.encode(staff.getId())));
+            students.forEach(student -> student.setPassword(passwordEncoder.encode(student.getId())));
+            userRepository.saveAll(students);
+            userRepository.saveAll(staffs);
+        }
         //Create default admin account
         String defaultAdminUsername = "admin001";
         String defaultAdminPassword = "123456";
